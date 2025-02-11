6866d0608338106e21a8c86c34f33042dba4fcd9
T T <1091539@qq.com>
2025-02-11 11:58:16 -0800

    open mock in test env

--- src/mockEnv.ts
+++ src/mockEnv.ts
@@ -1,9 +1,10 @@
 import { mockTelegramEnv, isTMA, parseInitData } from '@telegram-apps/sdk-react';
+import { Env, getEnv } from './utils/DashFunApi';
 
 // It is important, to mock the environment only for development purposes. When building the
 // application, import.meta.env.DEV will become false, and the code inside will be tree-shaken,
 // so you will not see it in your final bundle.
-if (import.meta.env.DEV) {
+if (import.meta.env.DEV || getEnv() != Env.Test) {
 	(() => {
 		let shouldMock;
 		const MOCK_KEY = '____mocked';
@@ -40,7 +41,7 @@ if (import.meta.env.DEV) {
 		// 	['chat_instance', '8428209589180549439'],
 		// ]).toString();
 
-    const initDataRaw = "query_id=AAGuFCQDAQAAAK4UJAPHYlhd&user=%7B%22id%22%3A2200179886%2C%22first_name%22%3A%22Marco%22%2C%22last_name%22%3A%22Test%22%2C%22username%22%3A%22Marco_web3%22%2C%22language_code%22%3A%22en%22%2C%22allows_write_to_pm%22%3Atrue%2C%22photo_url%22%3A%22https%3A%5C%2F%5C%2Fa-ttgme.stel.com%5C%2Fi%5C%2Fuserpic%5C%2F320%5C%2FUyoEyL4xrB_4jYbtDAKsRlU3-VHl9vlJ_ESwtdo9ztBcHXdXsAzLHk1biir38TJ-.svg%22%7D&auth_date=1738643830&signature=b9BaT5t8w5bR4b0S509qY1VIsmk20aymkhP2CfRv-r7M-G4Injb0iELE4licSA9F-nFaYPYkFFZQl-3IPf4jDg&hash=031c3c0040a6d79f979fab7c4c1db26fa7f67c9edc012fdfabcc64b8a14012e3"
+		const initDataRaw = "query_id=AAGuFCQDAQAAAK4UJAPHYlhd&user=%7B%22id%22%3A2200179886%2C%22first_name%22%3A%22Marco%22%2C%22last_name%22%3A%22Test%22%2C%22username%22%3A%22Marco_web3%22%2C%22language_code%22%3A%22en%22%2C%22allows_write_to_pm%22%3Atrue%2C%22photo_url%22%3A%22https%3A%5C%2F%5C%2Fa-ttgme.stel.com%5C%2Fi%5C%2Fuserpic%5C%2F320%5C%2FUyoEyL4xrB_4jYbtDAKsRlU3-VHl9vlJ_ESwtdo9ztBcHXdXsAzLHk1biir38TJ-.svg%22%7D&auth_date=1738643830&signature=b9BaT5t8w5bR4b0S509qY1VIsmk20aymkhP2CfRv-r7M-G4Injb0iELE4licSA9F-nFaYPYkFFZQl-3IPf4jDg&hash=031c3c0040a6d79f979fab7c4c1db26fa7f67c9edc012fdfabcc64b8a14012e3"
 		mockTelegramEnv({
 			themeParams: {
 				accentTextColor: '#3e88f7',
@@ -48,7 +49,7 @@ if (import.meta.env.DEV) {
 				buttonColor: '#3e88f7',
 				buttonTextColor: '#ffffff',
 				destructiveTextColor: '#eb5545',
-				headerBgColor: '#1a1a1a', 
+				headerBgColor: '#1a1a1a',
 				hintColor: '#98989e',
 				linkColor: '#3e88f7',
 				secondaryBgColor: '#1c1c1d',
