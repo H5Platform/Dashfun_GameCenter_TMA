2b93aa3cb4d46178794543f8264e36307e29887b
T T <1091539@qq.com>
2025-03-06 12:04:36 -0800

    update gamesavemgr

--- src/utils/DashFunApi.tsx
+++ src/utils/DashFunApi.tsx
@@ -10,7 +10,8 @@ enum Env {
 
 let env: Env = Env.Test
 
-const api_local = "https://tma-server-test.nexgami.com/api/v1/"
+// const api_local = "https://tma-server-test.nexgami.com/api/v1/"
+const api_local = "http://localhost:8080/api/v1/"
 const api_test = "https://dashfun-server-test.nexgami.com/api/v1/"
 const api_prod = "https://tma-server.dashfun.games/api/v1/"
 
